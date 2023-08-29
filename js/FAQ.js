58ad7a3f2332f46d0500e9cd3ceefcf6e4dc91e2
parksoohee <142369774+parksoohee@users.noreply.github.com>
2023-08-29 17:34:00 +0900

    Add files via upload

---
+++ js/FAQ.js
@@ -0,0 +1,23 @@
+$(function(){
+	$(".qna> ul> li").click(function(){
+		$(this).children(".text_info").slideToggle();
+		$(this).siblings().children(".text_info").slideUp();
+	});
+});
+$(function(){
+	$(".qna> ul> li").click(function(){
+		$(this).toggleClass("turn");
+		
+		if($(this).hasClass("turn")===true){
+				$(this).siblings().removeClass("turn");
+			
+		}
+
+	});
+});
+
+
+
+
+
+
